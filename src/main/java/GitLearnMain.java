e056e0df22c7b79b0985a68582a9a6af453abfdd
zhx <672062920@qq.com>
2019-08-29 14:31:16 +0800

    git add的时候为什么不让人指定文件夹

---
+++ src/main/java/GitLearnMain.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhx
+ * @date 2019/8/29
+ */
+public class GitLearnMain {
+    public static void main(String[] args) {
+        System.out.println("i am a littlebai first learn git");
+    }
+}
