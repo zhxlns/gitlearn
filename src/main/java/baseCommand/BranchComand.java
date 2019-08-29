494600d624676673714cb2b5bf6d8cca912a5e74
zhx <672062920@qq.com>
2019-08-29 15:54:27 +0800

    添加相关指令

---
+++ src/main/java/baseCommand/BranchComand.java
@@ -0,0 +1,9 @@
+package baseCommand;
+
+/**
+ * @author zhx
+ * @date 2019/8/29
+ */
+public class BranchComand {
+    public static final String command0 = "git switch";
+}
--- src/main/java/baseCommand/Command.java
+++ src/main/java/baseCommand/Command.java
@@ -13,4 +13,9 @@ public class Command {
     public static final String command2 = "git commit";
     //将本地仓库推送到远程仓库
     public static final String command3 = "git push";
+
+    //是一个具有多种用法的命令，实际上是用版本库中的文件替代工作区的文件
+    public static final String command4 = "git checkout";
+
+    //git对于分支有很强大的功能支持
 }
